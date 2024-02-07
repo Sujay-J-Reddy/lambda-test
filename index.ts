012431fcc1a0d0245e53e280349a06a1d351a582
Sujay-J-Reddy <sujayj1522002@gmail.com>
2024-02-07 16:23:20 +0530

    lodash removed

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda";
-import * as _ from 'lodash';
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
     const max = 10;
-    const val = _.random(max);
     const response = {
     statusCode: 200,
-    body: `The random vaule (max ${max}) is: ${val}`,
+    body: `The max value: ${max}`,
   };
   return response;
 };

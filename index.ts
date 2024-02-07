71fade290b8ac7aec6f6f49d062e6944cdd3e7f1
Sujay-J-Reddy <sujayj1522002@gmail.com>
2024-02-07 16:46:49 +0530

    test max=100

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda";
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
-    const max = 10;
+    const max = 100;
     const response = {
     statusCode: 200,
     body: `The max value: ${max}`,
